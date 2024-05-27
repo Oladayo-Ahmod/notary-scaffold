d200347f11352219223671c921aafc8305bc0b3d
Oladayo Ahmod <oladayoahmod112@gmail.com>
2024-05-27 05:40:46 +0100

    Initialize subgraph

---
+++ packages/subgraph/notary/src/notary.ts
@@ -0,0 +1,57 @@
+import {
+  DocumentNotarized as DocumentNotarizedEvent,
+  DocumentRetrieved as DocumentRetrievedEvent,
+  DocumentRevoked as DocumentRevokedEvent
+} from "../generated/Notary/Notary"
+import {
+  DocumentNotarized,
+  DocumentRetrieved,
+  DocumentRevoked
+} from "../generated/schema"
+
+export function handleDocumentNotarized(event: DocumentNotarizedEvent): void {
+  let entity = new DocumentNotarized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.documentHash = event.params.documentHash
+  entity.timestamp = event.params.timestamp
+  entity.imageURI = event.params.imageURI
+  entity.description = event.params.description
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDocumentRetrieved(event: DocumentRetrievedEvent): void {
+  let entity = new DocumentRetrieved(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.requester = event.params.requester
+  entity.documentHash = event.params.documentHash
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDocumentRevoked(event: DocumentRevokedEvent): void {
+  let entity = new DocumentRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.documentHash = event.params.documentHash
+  entity.timestamp = event.params.timestamp
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
