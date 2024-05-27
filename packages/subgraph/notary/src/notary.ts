7f11691472b1d245ca2176937ab4510ded4969a2
Oladayo Ahmod <oladayoahmod112@gmail.com>
2024-05-27 09:09:35 +0100

    complete and fix subgraph typeerror

--- packages/subgraph/notary/src/notary.ts
+++ packages/subgraph/notary/src/notary.ts
@@ -8,13 +8,19 @@ import {
   DocumentRetrieved,
   DocumentRevoked
 } from "../generated/schema"
+import { Address, BigInt,Bytes } from "@graphprotocol/graph-ts"
+
 
 export function handleDocumentNotarized(event: DocumentNotarizedEvent): void {
   let entity = new DocumentNotarized(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.owner = event.params.owner
-  entity.documentHash = event.params.documentHash
+  // Assuming event.params.documentHash is of type Bytes
+  let bytesValue: Bytes = event.params.documentHash;
+// Convert Bytes to a hex string 
+  let stringValue: string = bytesValue.toHex();
+  entity.documentHash = stringValue
   entity.timestamp = event.params.timestamp
   entity.imageURI = event.params.imageURI
   entity.description = event.params.description
@@ -31,7 +37,11 @@ export function handleDocumentRetrieved(event: DocumentRetrievedEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.requester = event.params.requester
-  entity.documentHash = event.params.documentHash
+  let bytesValue: Bytes = event.params.documentHash;
+  // Convert Bytes to a hex string 
+  let stringValue: string = bytesValue.toHex();
+// Assign the string value to the entity's documentHash
+  entity.documentHash = stringValue;
   entity.timestamp = event.params.timestamp
 
   entity.blockNumber = event.block.number
@@ -46,7 +56,11 @@ export function handleDocumentRevoked(event: DocumentRevokedEvent): void {
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
   entity.owner = event.params.owner
-  entity.documentHash = event.params.documentHash
+  let bytesValue: Bytes = event.params.documentHash;
+  // Convert Bytes to a hex string (example)
+  let stringValue: string = bytesValue.toHex();
+  // Assign the string value to the entity's documentHash
+  entity.documentHash = stringValue;
   entity.timestamp = event.params.timestamp
 
   entity.blockNumber = event.block.number
