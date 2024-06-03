3a938380bacecfcf0b0782a97064a099fdcfcccf
Oladayo Ahmod <oladayoahmod112@gmail.com>
2024-06-03 23:57:21 +0100

    integrate subgraph to query

--- packages/nextjs/app/blockexplorer/_components/ContractTabs.tsx
+++ packages/nextjs/app/blockexplorer/_components/ContractTabs.tsx
@@ -1,5 +1,4 @@
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
+// @ts-nocheck
 "use client";
 
 import { useEffect, useState } from "react";
@@ -12,36 +11,6 @@ import { createPublicClient, http } from "viem";
 import { hardhat } from "viem/chains";
 import { useFetchBlocks } from "~~/hooks/scaffold-eth";
 
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
-// .@ts-nocheck
-//·@ts-nocheck⏎⏎
-
 // @ts-nocheck
 
 // @ts-nocheck
@@ -64,26 +33,7 @@ import { useFetchBlocks } from "~~/hooks/scaffold-eth";
 
 // @ts-nocheck
 
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
-// @ts-nocheck
-
+// Define types for props
 type AddressCodeTabProps = {
   bytecode: string;
   assembly: string;
@@ -94,17 +44,22 @@ type PageProps = {
   contractData: AddressCodeTabProps | null;
 };
 
+// Create a public client for interacting with the blockchain
 const publicClient = createPublicClient({
   chain: hardhat,
   transport: http(),
 });
 
 export const ContractTabs = ({ address, contractData }: PageProps) => {
+  // Fetch blocks and transaction receipts, and manage pagination
   const { blocks, transactionReceipts, currentPage, totalBlocks, setCurrentPage } = useFetchBlocks();
+  // State to manage the active tab
   const [activeTab, setActiveTab] = useState("transactions");
+  // State to check if the address is a contract
   const [isContract, setIsContract] = useState(false);
 
   useEffect(() => {
+    // Check if the address is a contract by fetching the bytecode
     const checkIsContract = async () => {
       const contractCode = await publicClient.getBytecode({ address: address });
       setIsContract(contractCode !== undefined && contractCode !== "0x");
@@ -113,6 +68,7 @@ export const ContractTabs = ({ address, contractData }: PageProps) => {
     checkIsContract();
   }, [address]);
 
+  // Filter blocks to include only those with transactions involving the address
   const filteredBlocks = blocks.filter(block =>
     block.transactions.some(tx => {
       if (typeof tx === "string") {
